1786b946148c6fa48765e156728656a868d3035c
Taner Saydam <tanersaydam@gmail.com>
2022-06-22 20:21:11 +0300

    .

---
+++ Entities/Dtos/Auth.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class Auth : IDto
+    {
+        public User user { get; set; }
+        public List<OperationClaim> operationClaims { get; set; }
+    }
+}
